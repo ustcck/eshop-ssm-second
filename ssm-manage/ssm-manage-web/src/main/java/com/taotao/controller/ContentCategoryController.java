011dc1cd39208e892e3bd7ceaecb14ca36b67107
ustcck <ustcck@outlook.com>
2016-12-21 19:44:04 +0800

    fourth time

--- ssm-manage/ssm-manage-web/src/main/java/com/taotao/controller/ContentCategoryController.java
+++ ssm-manage/ssm-manage-web/src/main/java/com/taotao/controller/ContentCategoryController.java
@@ -4,7 +4,6 @@ import com.taotao.common.pojo.EasyUITreeNode;
 import com.taotao.common.pojo.TaotaoResult;
 import com.taotao.service.ContentCategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
